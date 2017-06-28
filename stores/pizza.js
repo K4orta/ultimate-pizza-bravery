64c8af5d8f4841f17300671eac7603121221a27d
K4Orta <k4orta@gmail.com>
2017-06-27 23:33:29 -0500

    Add pizza store and tests

--- stores/pizza.js
+++ stores/pizza.js
@@ -1,10 +1,38 @@
-import { extendObservable } from 'mobx'
+import { extendObservable, action } from 'mobx'
+
+function sideCode(left, right) {
+  if (left === right) {
+    return { '1/1': left ? left : '1' }
+  }
+
+  const enc = {}
+  if (left) {
+    enc['1/2'] = left
+  }
+
+  if (right) {
+    enc['2/2'] = right
+  }
+  return enc
+}
 
 export class Pizza {
   constructor(spec) {
     extendObservable(this, {
       crust: undefined,
       toppings: []
+    })
+  }
+
+  setCrust(crust) {
+    this.crust = crust
+  }
+
+  addTopping({ code, left, right}) {
+    const encodedSide = sideCode(left, right)
+    this.toppings.push({
+      code: code,
+      side: encodedSide,
     })
   }
 }
--- stores/pizza.test.js
+++ stores/pizza.test.js
@@ -6,3 +6,62 @@ test('It makes a pizza object', t => {
   t.true(p.crust === undefined)
   t.true(p.toppings.length === 0)
 })
+
+test('Can add a crust', t => {
+  const p = new Pizza()
+  p.setCrust('14SCREEN')
+  t.true(p.crust === '14SCREEN')
+})
+
+test('Can add a topping', t => {
+  const p = new Pizza()
+  p.addTopping({
+    code: 'C',
+  })
+  t.true(p.toppings.length === 1)
+  const firstTopping = p.toppings[0]
+  t.deepEqual(firstTopping, {
+    code: 'C',
+    side: {'1/1' : '1'},
+  })
+})
+
+test('Can add topping to only one side', t => {
+  const p = new Pizza()
+  p.addTopping({
+    code: 'P',
+    left: '1',
+  })
+  const leftTopping = p.toppings[0]
+  t.deepEqual(leftTopping, {
+    code: 'P',
+    side: {'1/2': '1'},
+  })
+
+  p.addTopping({
+    code: 'Z',
+    right: '1',
+  })
+  const rightTopping = p.toppings[1]
+  t.deepEqual(rightTopping, {
+    code: 'Z',
+    side: {'2/2': '1'},
+  })
+})
+
+test('Can add different amounts of toppings', t => {
+  const p = new Pizza()
+  p.addTopping({
+    code: 'C',
+    left: '0',
+    right: '1.5'
+  })
+  const topping = p.toppings[0]
+  t.deepEqual(topping, {
+    code: 'C',
+    side: {
+      '1/2': '0',
+      '2/2': '1.5'
+    },
+  })
+})
